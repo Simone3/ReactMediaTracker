5045c8e3ad551cd64f264797d96c4fd7b12db27f
Simone Graziussi <simone.graziussi@gmail.com>
2019-06-08 12:08:57 +0200

    Extract redux initialization in separate file

--- react-media-tracker/app/app.tsx
+++ react-media-tracker/app/app.tsx
@@ -1,14 +1,10 @@
 import React, { Component, ReactNode } from 'react';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
-import { rootReducer } from 'app/reducers/root';
 import { RootComponent } from 'app/components/root';
-import { rootSaga } from 'app/sagas/root';
+import { initializeRedux } from 'app/initializers/redux';
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(rootSaga);
+// Initialize app components
+const store = initializeRedux();
 
 /**
  * App entry point
