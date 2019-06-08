4018d117e2bd2433be5482c22cb762789901699b
Simone Graziussi <simone.graziussi@gmail.com>
2019-06-08 12:09:33 +0200

    Add first back-end configurations

--- react-media-tracker/app/config/config-sample.ts
+++ react-media-tracker/app/config/config-sample.ts
@@ -4,5 +4,13 @@ import { Config } from 'app/config/type-config';
  * Sample configuration that can be used as a template for other files, see config.ts
  */
 export const sampleConfig: Config = {
-	
+	tempToDelete: {
+		userId: '<temp_user_id>'
+	},
+	backEnd: {
+		baseUrl: '<backend_server_url>',
+		categories: {
+			get: '/users/:userId/categories'
+		}
+	}
 };
--- react-media-tracker/app/config/type-config.ts
+++ react-media-tracker/app/config/type-config.ts
@@ -2,5 +2,13 @@
  * Type for configuration files
  */
 export type Config = {
-	
+	tempToDelete: {
+		userId: string;
+	};
+	backEnd: {
+		baseUrl: string;
+		categories: {
+			get: string;
+		};
+	};
 }
