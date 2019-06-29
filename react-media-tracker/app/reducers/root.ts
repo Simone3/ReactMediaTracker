4a6d9b8e44c1f5247ef720a201da5431fb9ec127
Simone <simone.graziussi@gmail.com>
2019-06-29 17:18:10 +0200

    Move category reducers in inner folder

--- react-media-tracker/app/reducers/root.ts
+++ react-media-tracker/app/reducers/root.ts
@@ -1,6 +1,6 @@
 import { State } from 'app/models/internal/state';
-import { categoriesList } from 'app/reducers/categories-list';
-import { categoryDetails } from 'app/reducers/category-details';
+import { categoriesList } from 'app/reducers/category/categories-list';
+import { categoryDetails } from 'app/reducers/category/category-details';
 import { error } from 'app/reducers/error';
 import { Action, combineReducers } from 'redux';
 
