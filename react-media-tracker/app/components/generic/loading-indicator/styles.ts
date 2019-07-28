43b11d2fc75ede18e2029300d0c1772c973b73c8
Simone Graziussi <simone.graziussi@gmail.com>
2019-07-28 18:17:11 +0200

    Add loading indicator, for loading modal

---
+++ react-media-tracker/app/components/generic/loading-indicator/styles.ts
@@ -0,0 +1,16 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 0,
+		alignItems: 'center',
+		justifyContent: 'center',
+
+		height: Dimensions.get('window').height,
+
+		backgroundColor: 'transparent'
+	},
+	indicator: {
+		
+	}
+});
