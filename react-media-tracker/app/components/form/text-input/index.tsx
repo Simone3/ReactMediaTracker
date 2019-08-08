e6d4b2e0c72238ef0b588ca246e48735d3ece3cc
Simone <simone.graziussi@gmail.com>
2019-08-08 08:42:23 +0200

    Add component for form text input

---
+++ react-media-tracker/app/components/form/text-input/index.tsx
@@ -0,0 +1,66 @@
+import { styles } from 'app/components/form/text-input/styles';
+import React, { ReactNode, Component } from 'react';
+import { View, TextInput, ImageRequireSource, Image } from 'react-native';
+import { FieldComponent } from 'app/components/form/field';
+
+/**
+ * Presentational component to display a text input with Formik
+ */
+export class TextInputComponent extends Component<TextInputComponentInput> {
+	
+	/**
+	 * @override
+	 */
+	public render(): ReactNode {
+
+		const {
+			name,
+			placeholder,
+			icon
+		} = this.props;
+
+		return (
+			<FieldComponent name={name}>
+				{(field) => {
+					return (
+						<View style={styles.container}>
+							<Image
+								source={icon}
+								style={styles.icon}
+							/>
+							<TextInput
+								onChangeText={field.onChange}
+								onFocus={field.onFocus}
+								onBlur={field.onBlur}
+								value={field.value}
+								placeholder={placeholder}
+								style={styles.input}
+							/>
+						</View>
+					);
+				}}
+			</FieldComponent>
+		);
+	}
+}
+
+/**
+ * TextInputComponent's input props
+ */
+export type TextInputComponentInput = {
+
+	/**
+	 * The input name (unique in the form)
+	 */
+	name: string;
+
+	/**
+	 * The text placeholder
+	 */
+	placeholder: string;
+
+	/**
+	 * The input icon
+	 */
+	icon: ImageRequireSource;
+}
---
+++ react-media-tracker/app/components/form/text-input/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 0,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	icon: {
+		padding: 10,
+
+		width: 30,
+		height: 30
+	},
+	input: {
+		flex: 1,
+		padding: 10,
+		fontSize: 15
+	}
+});
