08f8bb3a0aa44257319700e4015040e4e1e141cc
Simone <simone.graziussi@gmail.com>
2019-08-08 08:41:31 +0200

    Add component for header with icon

---
+++ react-media-tracker/app/components/generic/header-title/index.tsx
@@ -0,0 +1,103 @@
+import { styles } from 'app/components/generic/header-title/styles';
+import React, { Component, ReactNode } from 'react';
+import { View, Image, Text, TouchableOpacity, ImageRequireSource } from 'react-native';
+
+/**
+ * Presentational component to display the header title, optionally with a clickable icon
+ */
+export class HeaderTitleComponent extends Component<HeaderTitleComponentInput & HeaderTitleComponentOutput> {
+	
+	/**
+	 * @override
+	 */
+	public render(): ReactNode {
+
+		const {
+			title
+		} = this.props;
+
+		return (
+			<View style={styles.container}>
+				<Text style={styles.title}>{title}</Text>
+				{this.renderIcon()}
+			</View>
+		);
+	}
+
+	/**
+	 * Helper to render the icon
+	 * @returns the icon
+	 */
+	private renderIcon(): ReactNode {
+
+		const {
+			icon,
+			iconClickStatus,
+			onIconClick
+		} = this.props;
+
+		if(icon) {
+
+			if(!iconClickStatus || iconClickStatus === 'NOT_CLICKABLE') {
+
+				return (
+					<Image
+						source={icon}
+						style={styles.icon}
+					/>
+				);
+			}
+			else {
+
+				const iconClickEnabled = iconClickStatus === 'ENABLED';
+
+				return (
+					<TouchableOpacity
+						onPress={onIconClick}
+						disabled={!iconClickEnabled}>
+						<Image
+							source={icon}
+							style={iconClickEnabled ? styles.icon : [ styles.icon, styles.iconDisabled ]}
+						/>
+					</TouchableOpacity>
+				);
+			}
+		}
+		else {
+
+			return null;
+		}
+	}
+}
+
+/**
+ * HeaderTitle's input props
+ */
+export type HeaderTitleComponentInput = {
+
+	/**
+	 * The header title
+	 */
+	title: string;
+
+	/**
+	 * The optional header icon
+	 */
+	icon?: ImageRequireSource;
+
+	/**
+	 * The icon click status
+	 */
+	iconClickStatus?: 'NOT_CLICKABLE' | 'ENABLED' | 'DISABLED';
+}
+
+/**
+ * HeaderTitle's output props
+ */
+export type HeaderTitleComponentOutput = {
+
+	/**
+	 * The icon click callback, called only if clickable and enabled
+	 */
+	onIconClick?: () => void;
+}
---
+++ react-media-tracker/app/components/generic/header-title/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	title: {
+		fontSize: 20,
+		flex: 1,
+		alignSelf: 'center',
+		margin: 10,
+		marginLeft: 15,
+		color: 'white',
+		fontWeight: 'bold'
+	},
+	icon: {
+		margin: 10,
+		marginRight: 15,
+		tintColor: 'white',
+		alignSelf: 'center',
+		width: 30,
+		height: 30
+	},
+	iconDisabled: {
+		opacity: 0.5
+	}
+});
