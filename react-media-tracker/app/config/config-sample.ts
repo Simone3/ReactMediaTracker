b4ad4fa01290f9771f03192734a48e2ee4e95291
Simone <simone.graziussi@gmail.com>
2019-08-08 08:38:58 +0200

    Add new config and temporary images

--- react-media-tracker/app/config/config-sample.ts
+++ react-media-tracker/app/config/config-sample.ts
@@ -15,7 +15,17 @@ export const sampleConfig: Config = {
 		colors: {
 			colorPrimary: '#3F51B5',
 			colorPrimaryDark: '#303F9F',
-			colorAccent: '#408cff'
+			colorAccent: '#408cff',
+			availableCategoryColors: [
+				'#3c82eb',
+				'#f25a5a',
+				'#74eb74',
+				'#ee9b52',
+				'#f5e064',
+				'#e75fe7',
+				'#4bead7',
+				'#6e6d66'
+			]
 		}
 	},
 	mocks: {
--- react-media-tracker/app/config/type-config.ts
+++ react-media-tracker/app/config/type-config.ts
@@ -14,6 +14,7 @@ export type Config = {
 			colorPrimary: string;
 			colorPrimaryDark: string;
 			colorAccent: string;
+			availableCategoryColors: string[];
 		};
 	};
 	mocks: {
