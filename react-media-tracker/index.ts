12ff49338b7c97b83764f7a4d11a2b8e7ff1afb5
Simone <simone.graziussi@gmail.com>
2019-09-15 19:00:11 +0200

    Add ignore warnings for the remote debugger

--- react-media-tracker/index.ts
+++ react-media-tracker/index.ts
@@ -1,8 +1,10 @@
 // eslint-disable-next-line import/no-unassigned-import
 import 'reflect-metadata';
 import { App } from 'app/app';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import { name as appName } from './app.json';
+
+YellowBox.ignoreWarnings([ 'Remote debugger' ]);
 
 AppRegistry.registerComponent(appName, () => {
 	return App;
