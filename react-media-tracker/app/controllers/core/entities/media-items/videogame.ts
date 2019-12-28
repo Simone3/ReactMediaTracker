a4d584c5018c510b7a32f5a6cfcf7c969bee1185
Simone <simone.graziussi@gmail.com>
2019-12-28 14:25:23 +0100

    Add back-end controller implementation for videogames

--- react-media-tracker/app/controllers/core/entities/media-items/videogame.ts
+++ react-media-tracker/app/controllers/core/entities/media-items/videogame.ts
@@ -1,6 +1,7 @@
+import { config } from 'app/config/config';
 import { MediaItemCatalogController, MediaItemController, MediaItemDefinitionsController } from 'app/controllers/core/entities/media-items/media-item';
 import { VideogameMockedCatalogController, VideogameMockedController } from 'app/controllers/impl-mocks/entities/media-items/videogame';
-import { VideogameDefinitionsControllerImpl } from 'app/controllers/impl-prod/entities/media-items/videogame';
+import { VideogameBackEndController, VideogameCatalogBackEndController, VideogameDefinitionsControllerImpl } from 'app/controllers/impl-prod/entities/media-items/videogame';
 import { CatalogVideogameInternal, SearchVideogameCatalogResultInternal, VideogameFilterInternal, VideogameInternal, VideogameSortByInternal } from 'app/data/models/internal/media-items/videogame';
 
 /**
@@ -24,12 +25,12 @@ export type VideogameDefinitionsController = MediaItemDefinitionsController<Vide
 /**
  * Singleton implementation of the videogame controller
  */
-export const videogameController: VideogameController = new VideogameMockedController();
+export const videogameController: VideogameController = config.mocks.mediaItems ? new VideogameMockedController() : new VideogameBackEndController();
 
 /**
  * Singleton implementation of the videogame catalog controller
  */
-export const videogameCatalogController: VideogameCatalogController = new VideogameMockedCatalogController();
+export const videogameCatalogController: VideogameCatalogController = config.mocks.mediaItems ? new VideogameMockedCatalogController() : new VideogameCatalogBackEndController();
 
 /**
  * Singleton implementation of the videogame definitions controller
--- react-media-tracker/app/controllers/impl-prod/entities/media-items/videogame.ts
+++ react-media-tracker/app/controllers/impl-prod/entities/media-items/videogame.ts
@@ -1,5 +1,158 @@
-import { VideogameDefinitionsController } from 'app/controllers/core/entities/media-items/videogame';
-import { DEFAULT_VIDEOGAME, VideogameFilterInternal, VideogameInternal, VideogameSortByInternal } from 'app/data/models/internal/media-items/videogame';
+import { config } from 'app/config/config';
+import { backEndInvoker } from 'app/controllers/core/common/back-end-invoker';
+import { VideogameCatalogController, VideogameController, VideogameDefinitionsController } from 'app/controllers/core/entities/media-items/videogame';
+import { videogameCatalogDetailsMapper, videogameCatalogSearchMapper, videogameFilterMapper, videogameMapper } from 'app/data/mappers/media-items/videogame';
+import { AddMediaItemResponse, DeleteMediaItemResponse, UpdateMediaItemResponse } from 'app/data/models/api/media-items/media-item';
+import { AddVideogameRequest, FilterVideogamesRequest, FilterVideogamesResponse, GetVideogameFromCatalogResponse, SearchVideogameCatalogResponse, SearchVideogamesRequest, SearchVideogamesResponse, UpdateVideogameRequest } from 'app/data/models/api/media-items/videogame';
+import { CatalogVideogameInternal, DEFAULT_VIDEOGAME, SearchVideogameCatalogResultInternal, VideogameFilterInternal, VideogameInternal, VideogameSortByInternal } from 'app/data/models/internal/media-items/videogame';
+import { miscUtils } from 'app/utilities/misc-utils';
+
+/**
+ * Implementation of the VideogameController that queries the back-end APIs
+ * @see VideogameController
+ */
+export class VideogameBackEndController implements VideogameController {
+
+	/**
+	 * @override
+	 */
+	public async filter(userId: string, categoryId: string, filter?: VideogameFilterInternal): Promise<VideogameInternal[]> {
+		
+		const request: FilterVideogamesRequest = {
+			filter: filter ? videogameFilterMapper.toExternal(filter) : undefined
+		};
+
+		const response = await backEndInvoker.invoke({
+			method: 'POST',
+			url: miscUtils.buildUrl([ config.backEnd.baseUrl, '/users/:userId/categories/:categoryId/videogames/filter' ], {
+				userId: userId,
+				categoryId: categoryId
+			}),
+			requestBody: request,
+			responseBodyClass: FilterVideogamesResponse
+		});
+		
+		return videogameMapper.toInternalList(response.videogames);
+	}
+
+	/**
+	 * @override
+	 */
+	public async search(userId: string, categoryId: string, searchTerm: string): Promise<VideogameInternal[]> {
+
+		const request: SearchVideogamesRequest = {
+			searchTerm: searchTerm,
+			filter: undefined
+		};
+
+		const response = await backEndInvoker.invoke({
+			method: 'POST',
+			url: miscUtils.buildUrl([ config.backEnd.baseUrl, '/users/:userId/categories/:categoryId/videogames/search' ], {
+				userId: userId,
+				categoryId: categoryId
+			}),
+			requestBody: request,
+			responseBodyClass: SearchVideogamesResponse
+		});
+		
+		return videogameMapper.toInternalList(response.videogames);
+	}
+	
+	/**
+	 * @override
+	 */
+	public async save(userId: string, categoryId: string, videogame: VideogameInternal): Promise<void> {
+
+		if(videogame.id) {
+
+			const request: UpdateVideogameRequest = {
+				videogame: videogameMapper.toExternal(videogame)
+			};
+	
+			await backEndInvoker.invoke({
+				method: 'PUT',
+				url: miscUtils.buildUrl([ config.backEnd.baseUrl, '/users/:userId/categories/:categoryId/videogames/:id' ], {
+					userId: userId,
+					categoryId: categoryId,
+					id: videogame.id
+				}),
+				requestBody: request,
+				responseBodyClass: UpdateMediaItemResponse
+			});
+		}
+		else {
+
+			const request: AddVideogameRequest = {
+				newVideogame: videogameMapper.toExternal(videogame)
+			};
+	
+			await backEndInvoker.invoke({
+				method: 'POST',
+				url: miscUtils.buildUrl([ config.backEnd.baseUrl, '/users/:userId/categories/:categoryId/videogames' ], {
+					userId: userId,
+					categoryId: categoryId
+				}),
+				requestBody: request,
+				responseBodyClass: AddMediaItemResponse
+			});
+		}
+	}
+
+	/**
+	 * @override
+	 */
+	public async delete(userId: string, categoryId: string, videogameId: string): Promise<void> {
+
+		await backEndInvoker.invoke({
+			method: 'DELETE',
+			url: miscUtils.buildUrl([ config.backEnd.baseUrl, '/users/:userId/categories/:categoryId/videogames/:id' ], {
+				userId: userId,
+				categoryId: categoryId,
+				id: videogameId
+			}),
+			responseBodyClass: DeleteMediaItemResponse
+		});
+	}
+}
+
+/**
+ * Implementation of the VideogameCatalogController that queries the back-end APIs
+ * @see VideogameCatalogController
+ */
+export class VideogameCatalogBackEndController implements VideogameCatalogController {
+
+	/**
+	 * @override
+	 */
+	public async search(searchTerm: string): Promise<SearchVideogameCatalogResultInternal[]> {
+
+		const response = await backEndInvoker.invoke({
+			method: 'GET',
+			url: miscUtils.buildUrl([ config.backEnd.baseUrl, '/catalog/videogames/search/:searchTerm' ], {
+				searchTerm: searchTerm
+			}),
+			responseBodyClass: SearchVideogameCatalogResponse
+		});
+		
+		return videogameCatalogSearchMapper.toInternalList(response.searchResults);
+	}
+
+	/**
+	 * @override
+	 */
+	public async getDetails(catalogId: string): Promise<CatalogVideogameInternal> {
+
+		const response = await backEndInvoker.invoke({
+			method: 'GET',
+			url: miscUtils.buildUrl([ config.backEnd.baseUrl, '/catalog/videogames/:catalogId' ], {
+				catalogId: catalogId
+			}),
+			responseBodyClass: GetVideogameFromCatalogResponse
+		});
+		
+		return videogameCatalogDetailsMapper.toInternal(response.catalogVideogame);
+	}
+}
 
 /**
  * Production implementation of the VideogameDefinitionsController
